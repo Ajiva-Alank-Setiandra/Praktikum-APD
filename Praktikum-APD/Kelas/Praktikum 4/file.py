cc5ecffdf16723816fd68d2fd288b5771491084b
Ajiva-Alank-Setiandra <ajiva015andra@gmail.com>
2024-09-25 09:02:22 +0800

    fourth commit

---
+++ Praktikum-APD/Kelas/Praktikum 4/file.py
@@ -0,0 +1,52 @@
+# ulang = 10
+# for i in range(ulang):
+#     print(f"perulangan ke-{str(i)}") 
+
+# simpan = [12, "udin petot", 14.5, True, 'A'] 
+# for i in simpan: 
+#     print(i, end=' ')
+
+# for i in range(1, 4): 
+#     for j in range(1, 4):
+#         print(f"{i} x {j} = {i * j}") 
+#     print()
+
+# a = 10
+# b = 7
+
+# print(f"nilai a = {a} dan nilai b = {b}")
+
+# jawab = 'ya' 
+# hitung = 0 
+# while(jawab == 'ya'):
+#     hitung += 1 
+#     jawab = input("Ulang lagi tidak? ") 
+# print(f"Total perulangan: {hitung}")
+
+# hitung = 0 
+# while True: 
+#     hitung += 1 
+#     ulang = input("Masih Ingin Mengulang? ")
+#     if ulang == "tidak" or ulang =="Tidak": 
+#         break 
+# print(f"Total Perulangan: {hitung}")
+
+
+# print("Daftar bilangan ganjil dari 1-10") 
+# for item in range(10): 
+#     if item % 3 == 0: 
+#         continue 
+#     print(item)
+
+# bilangan = 0 
+# while True:
+#     angka = input("tambah lagi? ") 
+#     if angka < 0:
+#         break
+#     bilangan += angka
+# print(f"Total perhitungan: {bilangan}")
+
+# for i in range (1, 20, 3):
+#     if i % 2 == 0:
+#         continue
+#     print(i)
